ce4cf6892666bcf0d5eb5917de75ce465fe6e42d
AstaRom <astafev.roman.mail@gmail.com>
2021-01-27 20:05:18 +0500

    update: development time

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfCrutches")]
-[assembly: AssemblyCopyright("Copyright © Roman Starkov 2012-2020")]
+[assembly: AssemblyCopyright("Copyright © Roman Starkov 2012-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
